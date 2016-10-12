23b9acd9b385a1f32578d73e7e40d335e9cf690d
Jules Martin <email": null>
2016-10-11 19:28:47 -0700

    tests are all passing

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,10 +1,10 @@
+require_relative "soda"
 class SodaMachine
-  attr_reader :sodas, :cash, :list
+  attr_reader :sodas, :cash
 
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
-    @list = args
   end
 
   def current_inventory_count
@@ -15,13 +15,25 @@ class SodaMachine
     @sodas.each do |soda|
       if soda.brand == soda_brand
       return soda
-      else
       end
     end
     nil
   end
 
   def sell(soda_brand)
+     @sodas.each do |soda|
+      if soda.brand == soda_brand
+       @cash += soda.price
+        @cash
+     end
   end
+    @sodas.delete(find_soda(soda_brand))
+end
 
 end
+
+# pepsi = Soda.new(brand: 'Pepsi', price: 0.65)
+# vend = SodaMachine.new( sodas: pepsi, cash: 1 )
+# p vend
+# p vend.cash
+# p vend.sodas.price
